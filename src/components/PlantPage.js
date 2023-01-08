1737c82bfaeb883e4912a1b522643c357be8464c
chengying <hollye2558@gmail.com>
2023-01-08 15:35:42 -0500

    Completed assignment

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [filteredPlants, setFilteredPlants] = useState([])
+  
+  useEffect(()=>{
+  // fetch("http://localhost:6001/plants")
+  // .then(res=>res.json())
+  // .then((data)=>{
+  //   setPlants(data)
+  //   setFilteredPlants(data)
+  // })
+  updatePlants();
+
+},[])
+
+function updatePlants() {
+  fetch("http://localhost:6001/plants")
+  .then(res=>res.json())
+  .then((data)=>{
+    setPlants(data)
+    setFilteredPlants(data)
+  })
+}
+
+function toSearch(text){
+  const newPlants=plants.filter((plant)=>plant.name.toLowerCase().includes(text.toLowerCase()))
+  setFilteredPlants(newPlants)
+}
+
+function toSubmit(){
+  updatePlants();
+}
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm toSubmit={toSubmit}  />
+      <Search toSearch={toSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
